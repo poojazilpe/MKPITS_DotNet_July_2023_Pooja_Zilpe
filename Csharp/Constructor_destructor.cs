6d69f5c2fc5266b0e974548758f9c6375d296003
Pooja Zilpe <poojazilpe1992@gmail.com>
2023-11-07 17:21:15 +0530

    Constructor & destructor

---
+++ Csharp/Constructor_destructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_destructor
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Console.WriteLine("Constructor called ");
+        }
+        ~ Employee()
+        {
+            Console.WriteLine("Destructor called ");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();
+            Console.ReadKey();
+            Employee e1 = new Employee();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Csharp/method_overloading_different_type_param.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_oveloding_different_type_parameter
+{
+    class Calculate
+    {
+        public void addition(int n1,int n2)
+        {
+            int res = n1 + n2;
+            Console.WriteLine("Addition of 2 integer nos : " + res);
+        }
+        public void addition(float n1,float n2)
+        {
+            float res = n1 + n2;
+            Console.WriteLine("Addition of 2 float nos : " + res);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+            c.addition(2, 3);
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine("Now calling addition float nos ");
+            c.addition(2.3f, 3.3f);
+            Console.ReadKey();
+        }
+    }
+}
