7607813879dd7eb3128b4b0927dea9ab56b216fb
Pooja Zilpe <poojazilpe1992@gmail.com>
2023-12-02 23:19:26 +0530

    Exception Handling

---
+++ Csharp/Form_add_sub_mul.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_add_sub_mul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Calculate c = new Calculate();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            c.add(num1, num2);
+            label3.Text = "Result : " + c.res;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            c.sub(num1, num2);
+            label3.Text = "Result : " + c.res;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            c.mul(num1, num2);
+            label3.Text = "Result : " + c.res;
+        }
+    }
+}
