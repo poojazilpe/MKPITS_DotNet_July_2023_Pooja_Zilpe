626721e2ffad54aef8721078b00445ff481c4263
Pooja Zilpe <poojazilpe1992@gmail.com>
2023-12-08 09:10:15 +0530

    Form

---
+++ Csharp/Window_form/Form_tab_control.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_tab_control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name : " + textBox1.Text+"\n");
+            sb.Append("Date of Birth : " + maskedTextBox1.Text+"\n");
+            sb.Append("Nationality : " + textBox2.Text+"\n");
+            sb.Append("City : " + textBox3.Text+"\n");
+            sb.Append("Address : " + richTextBox1.Text+"\n");
+            sb.Append("Mother Tounge : " + textBox4.Text+"\n");
+            sb.Append("Qualification : " + textBox5.Text+"\n");
+            sb.Append("Address : " + textBox6.Text);
+            label12.Text = sb.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("First Name :" + textBox7.Text+"\n");
+            sb.Append("Last Name :" + textBox8.Text+"\n");
+            sb.Append("Father Name :" + textBox9.Text+"\n");
+            sb.Append("Mother Name :" + textBox10.Text+"\n");
+            sb.Append("Father Occupation :" + textBox11.Text+"\n");
+            sb.Append("Mother Occupation :" + textBox12.Text+"\n");
+            sb.Append("Date of Birth :" + maskedTextBox2.Text+"\n");
+            sb.Append("Qualification :" + textBox14.Text+"\n");
+            
+            sb.Append("Address :" + richTextBox2.Text);
+            string gender = null;
+            if(radioButton1.Checked)
+            {
+                gender = radioButton1.Text;
+            }
+            else if (radioButton2.Checked)
+            {
+                gender = radioButton2.Text;
+            }
+            else if (radioButton3.Checked)
+            {
+                gender = radioButton3.Text;
+            }
+            sb.Append("Gender :" + gender+"\n");
+            label22.Text = sb.ToString();
+        }
+    }
+}
---
+++ Csharp/Window_form/Text_Editor_Project.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Editor_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string fn = openFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if (fn.EndsWith("rtf"))
+                {
+                    richTextBox1.LoadFile(openFileDialog1.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("Can not open this file");
+                }
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string fn = saveFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if(fn.EndsWith("rtf"))
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName);
+                    MessageBox.Show("Save file sucessfully");
+                }
+                else
+                {
+                    MessageBox.Show("Can not save file this format");
+                }
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            this.Width = hScrollBar1.Value;
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            this.Height = vScrollBar1.Value;
+        }
+    }
+}
