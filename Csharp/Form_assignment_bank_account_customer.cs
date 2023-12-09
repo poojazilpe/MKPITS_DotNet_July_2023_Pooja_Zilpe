6f213e64a578b21e5fc458ece8733e8e97986e6d
Pooja Zilpe <poojazilpe1992@gmail.com>
2023-12-09 17:37:13 +0530

    Form

---
+++ Csharp/Form_assignment_bank_account_customer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Form_assignment_bank_account_customer
+{
+    public class Bank
+    {
+
+        List<Customer> cust;
+        public Bank()
+        {
+            cust = new List<Customer>();
+        }
+        public void addcustomer(Customer customer)
+        {
+            cust.Add(customer);
+        }
+        public void display()
+        {
+            Console.WriteLine(cust);
+        }
+
+    }
+    public class Customer
+    {
+        public string cname;
+        List<Account> acc;
+        public Customer(string cname)
+        {
+            this.cname = cname;
+            acc = new List<Account>();
+        }
+        public void addaccount(Account account)
+        {
+            acc.Add(account);
+        }
+
+       
+    }
+
+    public class Account
+    {
+        public int id;
+        public int bal = 1000;
+        public int amt;
+        public Account(int id, int amt)
+        {
+            this.id = id;
+            this.amt = amt;
+        }
+
+        public void deposit()
+        {
+            bal = bal + amt;
+        }
+        public void withdrawl()
+        {
+            bal = bal - amt;
+        }
+        public void display()
+        {
+            Console.WriteLine("Account Details");
+            
+                Console.WriteLine("Account No : " + id);
+                
+                Console.WriteLine("Balance is : " + bal);
+                Console.WriteLine("-------------------------------------");
+            
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            while (true)
+            {
+
+                Console.WriteLine("Enter Name of customer: ");
+                string cust1 = Console.ReadLine();
+                Console.WriteLine("Enter Account no: ");
+                int accno = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Amount: ");
+                int amt = Convert.ToInt32(Console.ReadLine());
+                Customer c1 = new Customer(cust1);
+                Account a1 = new Account(accno, amt);
+                Console.WriteLine("Do you want to deposit or withdrawl");
+                string choice = Console.ReadLine();
+                if (choice == "deposit")
+                {
+                    a1.deposit();
+                    a1.display();
+
+                }
+                else if (choice == "withdrawl")
+                {
+                    a1.withdrawl();
+                    a1.display();
+
+                }
+
+            }
+
+            
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Csharp/machine_test_animal.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace machine_test_animal
+{
+    class Animal
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+        public Animal(int age,string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public void makesound()
+        {
+            Console.WriteLine("Animal make sound");
+        }
+        
+    }
+    class Mammal:Animal
+    {
+        public string furcolor { get; set; }
+        public Mammal(int age, string name, string furcolor) : base(age, name)
+        {
+            
+            this.furcolor = furcolor;
+        }
+        public void walk()
+        {
+            Console.WriteLine("Mammal is walking");
+        }
+    }
+    class Reptile : Animal
+    {
+        public bool Iscoldloaded { get; set; }
+        public Reptile(int age, string name, bool Iscoldloaded) : base(age, name)
+        {
+            this.Iscoldloaded = Iscoldloaded;
+        }
+        public void croll()
+        {
+            Console.WriteLine("Reptile is crolling");
+        }
+
+    }
+    class Bird : Animal
+    {
+        public int wing { get; set; }
+        public Bird(int age, string name, int wing) : base(age, name)
+        {
+            this.wing = wing;
+        }
+        public void fly()
+        {
+            Console.WriteLine("Bird is flying");
+        }
+
+    }
+    class Lion : Mammal
+    {
+
+        public Lion(int age,string name, string furcolor) :base(age,name,furcolor)
+        {
+            this.age = age;
+            this.name = furcolor;
+        }
+        public void makesound()
+        {
+            Console.WriteLine("Lion Roars loudly");
+        }
+        public void display()
+        {
+            Console.WriteLine("Age : "+age+"\n");
+            Console.WriteLine("Name : " + name + "\n");
+            Console.WriteLine("Furcolor : " + furcolor + "\n");
+        }
+
+
+    }
+    class Snake :Reptile
+    {
+        public Snake(int age, string name, bool Iscoldloaded) : base(age, name, Iscoldloaded)
+        {
+            
+
+        }
+        public void makesound()
+        {
+            Console.WriteLine("Snake is hiss");
+        }
+        public void display()
+        {
+            Console.WriteLine("Age : " + age + "\n");
+            Console.WriteLine("Name : " + name + "\n");
+            Console.WriteLine("Iscoldloaded : " + Iscoldloaded + "\n");
+        }
+
+    }
+    class Eagle : Bird
+    {
+        public Eagle(int age, string name, int wing) : base(age, name, wing)
+        {
+
+
+        }
+        public void makesound()
+        {
+            Console.WriteLine("Eagle is scriches");
+        }
+        public void display()
+        {
+            Console.WriteLine("Age : " + age + "\n");
+            Console.WriteLine("Name : " + name + "\n");
+            Console.WriteLine("Wing : " + wing + "\n");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Lion l = new Lion(3,"simba","Orange");
+            l.display();
+            l.walk();
+            Snake s = new Snake(2,"Cobra",true);
+            s.display();
+            s.croll();
+            Eagle e = new Eagle(1, "Egloo", 2);
+            e.display();
+            e.fly();
+
+            
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+
